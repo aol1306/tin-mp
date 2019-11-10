ff87c5e2a9114f11d2de5bb1b9383afc1ccc28ee
aol1306 <jdzieciatko@gmail.com>
2019-11-10 21:57:11 +0100

    Add validation logic

--- js/validation.js
+++ js/validation.js
@@ -1,30 +1,50 @@
+// returns error message
 function validate(element) {
     var classes = element.classList
     var errorElement = element.nextElementSibling
-    var ok = true
+    var allErrors = ""
 
     if (isIn(classes, 'form-required')) {
         if (element.value < 1) {
-            addErrorMessage("This field is required", errorElement)
-            ok = false
+            var errorString = "Pole " + element.getAttribute("name") + " jest wymagane"
+            addErrorMessage(errorString, errorElement)
+            allErrors += errorString
         }
     }
+
     // if passed all checks
-    if (ok) {
+    if (allErrors == "") {
         clearErrorMessage(errorElement)
     }
 
-    return ok
+    return allErrors
 }
 
 function validateForm(element) {
     // validate all form elements
     // display combined error message
-    var children = element.getChildren
+    var children = element.childNodes
+    var errorBox = document.getElementById("error-box")
+    var allErrors = []
+
     for (var i=0; i<children.length; i++) {
-        if(isIn(children[i].classList, 'validate')) {
-            validate(children[i])
+        if(children[i].classList != undefined && isIn(children[i].classList, 'validate')) {
+            var validationResult = validate(children[i])
+            if (validationResult != "") {
+                allErrors.push(validationResult)
+            }
         }
+    }
+
+    console.log(allErrors)
+    if (allErrors.length == 0) {
+        return true
+    } else {
+        errorBox.innerHTML = ""
+        for (var i = 0; i < allErrors.length; i++) {
+            errorBox.innerHTML += allErrors[i] + "<br>"
+        }
+        return false
     }
 }
 
@@ -32,7 +52,7 @@ function addErrorMessage(msg, errorElement) {
     if (!isIn(errorElement.classList, 'error-message')) {
         console.log('error element does not contain error-message class!')
     }
-    errorElement.innerHTML = 'To pole jest wymagane'
+    errorElement.innerHTML = msg
 }
 
 function clearErrorMessage(errorElement) {
