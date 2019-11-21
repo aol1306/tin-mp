19cea358ed89eb2af5dc4b69fa6a92f5ec237887
aol1306 <jdzieciatko@gmail.com>
2019-11-21 21:48:50 +0100

    Add server program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tinmp
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+    log.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
+}
