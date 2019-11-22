048047bd58c8d4e20273119fae4412070bddfdbb
aol1306 <jdzieciatko@gmail.com>
2019-11-22 10:54:20 +0100

    Move handlers to separate functions

--- main.go
+++ main.go
@@ -6,73 +6,71 @@ import (
 	"net/http"
 )
 
-/*func logging(f http.HandlerFunc) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        log.Println(r.URL.Path)
-        f(w, r)
-    }
-}*/
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		tmpl := template.Must(template.ParseFiles("templates/login.html"))
+		tmpl.Execute(w, nil)
+	} else {
+		// TODO: verify login credentials
+		http.Redirect(w, r, "landing", http.StatusSeeOther)
+	}
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/register.html"))
+	tmpl.Execute(w, nil)
+}
+
+func viewcards(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/view-cards.html"))
+	tmpl.Execute(w, nil)
+}
+
+// TODO: add auth
+
+func landing(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/landing.html"))
+	tmpl.Execute(w, nil)
+}
+
+func edit(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/edit.html"))
+	tmpl.Execute(w, nil)
+}
+
+func details(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/details.html"))
+	tmpl.Execute(w, nil)
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/add.html"))
+	tmpl.Execute(w, nil)
+}
 
 func main() {
-    log.Println("Start")
+	log.Println("Start")
 
-	// index page
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		tmpl.Execute(w, nil)
-	})
-
-	// login page
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl := template.Must(template.ParseFiles("templates/login.html"))
-			tmpl.Execute(w, nil)
-		} else {
-			// TODO: verify login credentials
-			http.Redirect(w, r, "landing", http.StatusSeeOther)
-		}
-	})
-
-	// register page
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/register.html"))
-		tmpl.Execute(w, nil)
-	})
-
-    // view cards page
-	http.HandleFunc("/view-cards", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/view-cards.html"))
-		tmpl.Execute(w, nil)
-	})
-
-
-    // ***
-    // ONLY FOR AUTHORIZED USERS
-    // ***
-
-    // landing page
-    http.HandleFunc("/landing", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/landing.html"))
-		tmpl.Execute(w, nil)
-	})
-
-    // edit page
-    http.HandleFunc("/edit", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/edit.html"))
-		tmpl.Execute(w, nil)
-	})
-
-    // details page
-    http.HandleFunc("/details", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/details.html"))
-		tmpl.Execute(w, nil)
-	})
-
-    // add page
-    http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/add.html"))
-		tmpl.Execute(w, nil)
-	})
+	http.HandleFunc("/", logging(index))
+	http.HandleFunc("/login", logging(login))
+	http.HandleFunc("/register", logging(register))
+	http.HandleFunc("/viewcards", logging(viewcards))
+	http.HandleFunc("/landing", logging(landing))
+	http.HandleFunc("/edit", logging(edit))
+	http.HandleFunc("/details", logging(details))
+	http.HandleFunc("/add", logging(add))
 
 	// static files
 	fs := http.FileServer(http.Dir("static/"))
